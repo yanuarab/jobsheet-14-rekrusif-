0e81f4724ddb99ca97f8a3bf2400424a7c059e2f
tugas <aldayanuar6@gmail.com>
2024-11-28 19:29:02 +0700

    percobaan2

---
+++ Percobaan2.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Percobaan2 {
+
+    static int hitungPangkat(int x, int y) {
+        if (y == 0) {
+            return (1);
+        } else {
+            return (x * hitungPangkat(x, y - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int bilangan, pangkat;
+
+        System.out.print("Bilangan yang dihitung: ");
+        bilangan = sc.nextInt();
+        System.out.print("Pangkat: ");
+        pangkat = sc.nextInt();
+
+        System.out.println(hitungPangkat(bilangan, pangkat));
+        
+        sc.close();
+    }
+}
